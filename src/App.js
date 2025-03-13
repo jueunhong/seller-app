a5e399cd0717a983329acaa55cd90f3c65d644b7
2yunseong <dbsdltjd123@gmail.com>
2025-03-13 15:21:18 +0900

    feat: base URL 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Router from './Router.js';
 import { BrowserRouter } from 'react-router-dom';
 
-
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <Router/>
+    <BrowserRouter>
+      <Router />
     </BrowserRouter>
-   
   );
 }
 
